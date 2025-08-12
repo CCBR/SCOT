f077bbb0ba67a3771017a942d436f0a1452891a7
Nathan Wong <nathan.wong@nih.gov>
2025-08-12 09:15:34 -0400

    convert matrix running

--- R/convert_mtx_to_h5.R
+++ R/convert_mtx_to_h5.R
@@ -1,23 +1,22 @@
-#'convertMtxToH5: Converts the triple .mtx files from single cell experiments to a .h5 counts file
-#' 
-#' @description 
-#' 
-#' @details 
-#' 
-#' @param 
-#' 
-#' @return 
+#' convertMtxToH5: Converts the triple .mtx files from single cell experiments to a .h5 counts file
+#'
+#' @description
+#'
+#' @details
+#'
+#' @param
+#'
+#' @return
 
 library(Seurat)
 library(DropletUtils)
 
-args = commandArgs(trailingOnly=T)
-
-sampleName = args[1]
-mtx_file = args[2]
-features_file=args[3]
-barcodes_file=args[4]
-
-counts = Seurat::ReadMtx(mtx = mtx_file, cells = barcodes_file, features=features_file)
-outfile = paste0(sampleName,".h5")
-DropletUtils::write10xCounts(x= counts,path=outfile)
+convert_mtx_to_h5 <- function(
+    sample_name,
+    mtx_file,
+    features_file,
+    barcodes_file) {
+  counts <- Seurat::ReadMtx(mtx = mtx_file, cells = barcodes_file, features = features_file)
+  outfile <- paste0(sample_name, ".h5")
+  DropletUtils::write10xCounts(x = counts, path = outfile)
+}
