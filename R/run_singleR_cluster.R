9ce5354ca6f02dee7ba780b031cf59a5443157de
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-08-21 11:09:25 -0400

    fix: clashing imports from SingleR & celldex
    fixes #8

--- R/run_singleR_cluster.R
+++ R/run_singleR_cluster.R
@@ -8,20 +8,17 @@
 #' (SingleCellExperiment object)
 #' @param label A cell type label from the metadata column headers
 #'
-#' @import celldex
-#' @import SingleR
-#' 
-#' @export 
-#' 
+#' @export
+#'
 #' @return A character vector of matched cell type annotations based on
 #' clusters
 #'
 
 run_singleR_cluster <- function(so_in, ref_file, label) {
-  avg <- AverageExpression(so_in, assays = "SCT")
+  avg <- Seurat::AverageExpression(so_in, assays = "SCT")
   avg <- as.data.frame(avg)
-  ref <- refFile
-  s <- SingleR(test = as.matrix(avg), ref = ref, labels = ref[[label]])
+  ref <- refFile # TODO R CMD check: no visible binding for global variable ‘refFile’
+  s <- SingleR::SingleR(test = as.matrix(avg), ref = ref, labels = ref[[label]])
 
   clust_annot <- s$labels
   names(clust_annot) <- colnames(avg)
