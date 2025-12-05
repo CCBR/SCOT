1d15407bdc8e3db91259dc093efeffabe4c55c8e
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-12-05 16:27:53 -0500

    docs: create package doc
    allows `?SCOT` to work. in the future we can also add more info e.g. link to an intro vignette

---
+++ R/SCOT-package.R
@@ -0,0 +1,13 @@
+#' SCOT: Toolbox for single cell RNA-seq
+#'
+#' Designed for single-cell RNA-seq analysis with [SINCLAIR](https://github.com/CCBR/SINCLAIR).
+#'
+#' See the website for more information, documentation, and examples:
+#'   \url{https://ccbr.github.io/SCOT}
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
