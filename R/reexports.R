f9ad6ec0048c029a77de32e9dd695833d734afda
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-12-05 16:19:23 -0500

    fix: use Imports needed by Seurat

--- R/reexports.R
+++ R/reexports.R
@@ -17,3 +17,11 @@ rlang::`:=`
 #' @importFrom rlang .data
 #' @export
 rlang::.data
+
+
+# Suppress R CMD check note 'All declared Imports should be used'.
+# These packages are used by Seurat, but are only listed in Seurat's Suggests field.
+# See \url{https://community.rstudio.com/t/how-should-a-meta-package-handle-this-note-all-declared-imports-should-be-used/23400/3}
+#' @importFrom harmony RunHarmony
+#' @importFrom rliger createLiger
+NULL
