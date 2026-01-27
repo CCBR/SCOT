4945ac7d9ba5677e102b81648cda72f38be0186b
Nathan Wong <nathan.wong@nih.gov>
2026-01-27 16:15:24 -0500

    feature: cell filtering function with unit test and updated helper functions to match Seurat v5.

--- R/filter_cells.R
+++ R/filter_cells.R
@@ -3,7 +3,7 @@
 #' @description Wrapper function to filter cells from a Seurat object using
 #' method of choice. Returns a Seurat object with filtering results
 #' 
-#' @param so
+#' @param so Seurat object to filter
 #' @param method Filtering method to use: "miQC" (default), "manual", or "mads"
 #' @param mads Number of median absolute deviations to use for "mads" method (default 3)
 #' @param nCount_RNA_max Maximum nCount_RNA threshold for "manual" method (default 500000)
@@ -22,19 +22,22 @@
 #' - "mads": Filters cells based on median absolute deviations from the median for
 #' nCount_RNA, nFeature_RNA, and percent_mt
 #' 
+#' @return Returns a Seurat object with an additional metadata column "keep" indicating
+#' whether each cell is to be kept or discarded
+#' 
 #' @export
 #' 
 filter_cells <- function(so, method = "miQC",
   mads = 3,
-  nCount_RNA_max = NULL, nCount_RNA_min = NULL,
-  nFeature_RNA_max = NULL, nFeature_RNA_min = NULL,
-  percent_mt_max = NULL, percent_mt_min = NULL) {
-
-  so_filt <- Seurat::subset(so, subset = nFeature_RNA > 200) #standard low cell read filter
+  nCount_RNA_max = 50000, nCount_RNA_min = 1000,
+  nFeature_RNA_max = 5000, nFeature_RNA_min = 200,
+  percent_mt_max = 10, percent_mt_min = 0) {
+  nFeature_RNA <- NULL
+  so_filt <- BiocGenerics::subset(so, subset = nFeature_RNA > 200) #standard low cell read filter
   if (method == "miQC") {
     print("--filtering cells with miQC")
 
-    so_qc <- miQC::RunMiQC(so_filt,
+    so_qc <- SeuratWrappers::RunMiQC(so_filt,
       percent.mt = "percent.mt",
       nFeature_RNA = "nFeature_RNA",
       posterior.cutoff = 0.7,
@@ -45,12 +48,21 @@ filter_cells <- function(so, method = "miQC",
     so_qc <- so_filt
     so_qc$keep <- "discard"
     so_qc$keep[which(
-      nCount_RNA_max >= nCount_RNA & nCount_RNA >= nCount_RNA_min &
-      nFeature_RNA_max >= nFeature_RNA & nFeature_RNA >= nFeature_RNA_min &
-      percent_mt_max >= percent.mt & percent.mt >= percent_mt_min
+      so_qc$nCount_RNA <= nCount_RNA_max & so_qc$nCount_RNA >= nCount_RNA_min &
+      so_qc$nFeature_RNA <= nFeature_RNA_max & so_qc$nFeature_RNA >= nFeature_RNA_min &
+      so_qc$percent.mt <= percent_mt_max & so_qc$percent.mt >= percent_mt_min
     )] <- "keep"
   } else if (method == "mads") {
-     so_qc <- so_filt
+    so_qc <- so_filt
+    nCount_out <- Routliers::outliers_mad(so_qc$nCount_RNA, threshold = mads)$LL_CI_MAD
+    nFeature_out <- Routliers::outliers_mad(so_qc$nFeature_RNA, threshold = mads)$LL_CI_MAD
+    percent_mt_out <- Routliers::outliers_mad(so_qc$percent.mt, threshold = mads)$UL_CI_MAD
+    so_qc$keep <- "discard"
+    so_qc$keep[which(
+      so_qc$nCount_RNA >= nCount_out &
+      so_qc$nFeature_RNA >= nFeature_out &
+      so_qc$percent.mt <= percent_mt_out
+    )] <- "keep"
   }
 
   return(so_qc)
