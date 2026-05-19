f3b80b5a0736fe22005390a4571ce8a4dc3d5ace
Nathan Wong <nathan.wong@nih.gov>
2026-05-19 15:37:45 -0400

    Expected error messages added to test cases

--- tests/testthat/test-convert_human_gene_list.R
+++ tests/testthat/test-convert_human_gene_list.R
@@ -9,10 +9,16 @@ test_that("human_unique_genes", {
   input_human <- c("TP53", "SRGAP2C", "HYDIN2", "ARGAP11B")
   expect_equal(convert_human_gene_list(input_human), "Trp53")
   #Error in mouse-side retrieval if ONLY human-unique genes
-  expect_error(convert_human_gene_list(input_human[-1]))
+  expect_error(
+    convert_human_gene_list(input_human[-1]),
+    "None of the keys entered are valid keys for 'ENTREZID'"
+  )
 })
 
 test_that("no_valid_genes", {
   input_non_existent <- c("Aaaa", "abababa", "XKCD")
-  expect_error(convert_human_gene_list(input_non_existent))
+  expect_error(
+    convert_human_gene_list(input_non_existent),
+    "None of the keys entered are valid keys for 'SYMBOL'"
+  )
 })
